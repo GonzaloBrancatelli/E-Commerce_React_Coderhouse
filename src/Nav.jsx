ac00fd67961e54a5f36609ef03d55a19dc5df672
B4D_7R1P <gonzalobrancatelli@gmail.com>
2022-11-29 20:17:24 -0300

    first commit

---
+++ src/Nav.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Nav = () => {
+ 
+  return (
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="">Navbar</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav m-auto">
+              <a className="nav-link" href="">Home</a>
+              <a className="nav-link" href="">Features</a>
+              <a className="nav-link" href="">Pricing</a>
+              <a className="nav-link" href="">About us</a>
+            </div>
+            <div className="d-flex align-items-center gap-3">
+              <button className='btn btn-outline-light p-2'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
+                  <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                </svg>
+              </button>
+              <form className='d-flex gap-3' role="search">
+                <input className="form-control" type="search" placeholder="Search" aria-label="Search" />
+                <button className="btn btn-outline-success" type="submit">Search</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Nav;
