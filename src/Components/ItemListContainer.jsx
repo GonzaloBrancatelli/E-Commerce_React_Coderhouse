4f05bbc13c6bd434aef5fae855c1bb185e31245a
B4D_7R1P <gonzalobrancatelli@gmail.com>
2022-12-12 03:02:15 -0300

    Agregando ItemCount

--- src/Components/ItemListContainer.jsx
+++ src/Components/ItemListContainer.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import "../index.css";
+import ItemCount from './ItemCount';
 
 const ItemListContainer = ({greetings}) => {
     return(
        <div>
         <p className='alert alert-danger text-center'>{greetings}</p>
+        <div>
+            <ItemCount stockItems={10}/>
+        </div>
        </div>
     );
 }
